9f075544da7b656a1b4b1e79d49097590e290958
Rovinota <tanvir.cse8.bu@gmail.com>
2023-11-04 22:24:45 +0600

    Module 11 Live Test Complited.

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,35 @@
 import 'package:flutter/material.dart';
+import 'weather_model.dart';
 
 void main() => runApp(MyApp());
 
-class Weather {
-  final String city;
-  final int temperature;
-  final String condition;
-  final int humidity;
-  final double windSpeed;
-
-  Weather({
-    required this.city,
-    required this.temperature,
-    required this.condition,
-    required this.humidity,
-    required this.windSpeed,
-  });
-}
-
 class MyApp extends StatelessWidget {
-  final List<Weather> weatherData = [
-    Weather(city: "New York", temperature: 20, condition: "Clear", humidity: 60, windSpeed: 5.5),
-    Weather(city: "Los Angeles", temperature: 25, condition: "Sunny", humidity: 50, windSpeed: 6.8),
-    Weather(city: "London", temperature: 15, condition: "Partly Cloudy", humidity: 70, windSpeed: 4.2),
-    Weather(city: "Tokyo", temperature: 28, condition: "Rainy", humidity: 75, windSpeed: 8.0),
-    Weather(city: "Sydney", temperature: 22, condition: "Cloudy", humidity: 55, windSpeed: 7.3),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Weather App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Weather Information'),
-        ),
-        body: ListView.builder(
-          itemCount: weatherData.length,
-          itemBuilder: (context, index) {
-            Weather weather = weatherData[index];
-            return ListTile(
-              title: Text(weather.city),
-              subtitle: Text('Temperature: ${weather.temperature}°C\nCondition: ${weather.condition}\nHumidity: ${weather.humidity}%\nWind Speed: ${weather.windSpeed} km/h'),
-            );
-          },
-        ),
+      home: WeatherScreen(),
+    );
+  }
+}
+
+class WeatherScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Weather Information'),
+      ),
+      body: ListView.builder(
+        itemCount: weatherData.length,
+        itemBuilder: (context, index) {
+          var cityData = weatherData[index];
+          return ListTile(
+            title: Text(cityData['city']),
+            subtitle: Text(
+                'Temperature: ${cityData['temperature']}°C\nCondition: ${cityData['condition']}\nHumidity: ${cityData['humidity']}%\nWind Speed: ${cityData['windSpeed']} m/s'),
+          );
+        },
       ),
     );
   }
